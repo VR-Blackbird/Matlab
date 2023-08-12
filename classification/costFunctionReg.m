4412047ce3f288c6cf77e41bfcbf598aa7e39275
Venkataraman <venkataraman1503@gmail.com>
2023-08-12 22:37:05 +0530

    Regularized cost and gradient

--- classification/costFunctionReg.m
+++ classification/costFunctionReg.m
@@ -18,8 +18,15 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
-
-
+z = X * theta;      % m x 1
+h_x = sigmoid(z);  % m x 1 
+  
+reg_term = (lambda/(2*m)) * sum(theta(2:end).^2);
+  
+J = (1/m)*sum((-y.*log(h_x))-((1-y).*log(1-h_x))) + reg_term; % scalar
+  
+grad(1) = (1/m)* (X(:,1)'*(h_x-y));                                  % 1 x 1
+grad(2:end) = (1/m)* (X(:,2:end)'*(h_x-y))+(lambda/m)*theta(2:end);  % n x 1
 
 
 % =============================================================
